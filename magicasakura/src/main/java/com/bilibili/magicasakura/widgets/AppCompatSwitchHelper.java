b34e0b90139ba13350d293f4b8dbe69c7604f3be
夏赟丞 <xyczero617@gmail.com>
2017-05-24 12:34:46 +0800

    add support SwitchCompat widget

---
+++ magicasakura/src/main/java/com/bilibili/magicasakura/widgets/AppCompatSwitchHelper.java
@@ -0,0 +1,227 @@
+package com.bilibili.magicasakura.widgets;
+
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v7.widget.SwitchCompat;
+import android.util.AttributeSet;
+
+import com.bilibili.magicasakura.utils.DrawableUtils;
+import com.bilibili.magicasakura.utils.ThemeUtils;
+import com.bilibili.magicasakura.utils.TintInfo;
+import com.bilibili.magicasakura.utils.TintManager;
+
+/**
+ * @author xyczero617@gmail.com
+ * @since 17/5/23
+ */
+
+public class AppCompatSwitchHelper {
+
+    private int[] sAttrs;
+    private SwitchCompat mSwitchCompat;
+    private TintManager mTintManager;
+    private boolean mSkipNextApply;
+
+    private DrawableCallback mDrawableCallback;
+
+    private TintInfo mTintInfo;
+    private int mResId;
+    private int mTintResId;
+    private PorterDuff.Mode mTintMode;
+
+    public AppCompatSwitchHelper(SwitchCompat switchCompat, TintManager tintManager,
+                                 int[] attrs, @NonNull DrawableCallback callback) {
+        this.sAttrs = attrs;
+        this.mTintManager = tintManager;
+        this.mSwitchCompat = switchCompat;
+        this.mDrawableCallback = callback;
+    }
+
+    private boolean skipNextApply() {
+        if (mSkipNextApply) {
+            mSkipNextApply = false;
+            return true;
+        }
+        mSkipNextApply = true;
+        return false;
+    }
+
+    private void setSkipNextApply(boolean flag) {
+        mSkipNextApply = flag;
+    }
+
+    @SuppressWarnings("ResourceType")
+    public void loadFromAttribute(AttributeSet attrs, int defStyleAttr) {
+        TypedArray array = mSwitchCompat.getContext().obtainStyledAttributes(
+                attrs, sAttrs, defStyleAttr, 0);
+        if (array.hasValue(1)) {
+            mTintResId = array.getResourceId(1, 0);
+            if (array.hasValue(2)) {
+                setSupportDrawableTintMode(mTintMode = DrawableUtils.parseTintMode(array.getInt(2, 0), null));
+            }
+            setSupportDrawableTint(mTintResId);
+        } else {
+            Drawable drawable = mTintManager.getDrawable(mResId = array.getResourceId(0, 0));
+            if (drawable != null) {
+                setDrawable(drawable);
+            }
+        }
+        array.recycle();
+    }
+
+    /**
+     * External use
+     */
+    public void setDrawable() {
+        if (skipNextApply()) return;
+
+        resetTintResource(0);
+        setSkipNextApply(false);
+    }
+
+    public void setDrawableId(int resId) {
+        if (mResId != resId) {
+            resetTintResource(resId);
+
+            if (resId != 0) {
+                Drawable drawable = mTintManager.getDrawable(resId);
+                setDrawable(drawable != null
+                        ? drawable
+                        : ContextCompat.getDrawable(mSwitchCompat.getContext(), resId));
+            }
+        }
+    }
+
+    public void setDrawableTintList(ColorStateList origin) {
+        ColorStateList tint = ThemeUtils.getThemeColorStateList(mSwitchCompat.getContext(), origin);
+        if (mTintInfo == null) {
+            mTintInfo = new TintInfo();
+        }
+        mTintInfo.mHasTintList = true;
+        mTintInfo.mTintList = tint;
+        applySupportDrawableTint();
+    }
+
+    public void setDrawableTintMode(PorterDuff.Mode mode) {
+        if (mode != null && mode != mTintMode) {
+            if (mTintInfo != null) {
+                mTintInfo.mHasTintList = false;
+                mTintInfo.mTintList = null;
+            }
+            setSupportDrawableTintMode(mode);
+            setSupportDrawableTint(mTintResId);
+        }
+    }
+
+    public void setButtonDrawableTintList(int resId, PorterDuff.Mode mode) {
+        if (mTintResId != resId) {
+            mTintResId = resId;
+            if (mTintInfo != null) {
+                mTintInfo.mHasTintList = false;
+                mTintInfo.mTintList = null;
+                mTintInfo.mHasTintMode = false;
+                mTintInfo.mTintMode = null;
+            }
+            setSupportDrawableTintMode(mode);
+            setSupportDrawableTint(resId);
+        }
+    }
+
+    /**
+     * Internal use
+     */
+    private void setDrawable(Drawable drawable) {
+        if (skipNextApply()) return;
+
+        mDrawableCallback.setDrawable(drawable);
+    }
+
+    private boolean setSupportDrawableTint(int resId) {
+        if (resId != 0) {
+            if (mTintInfo == null) {
+                mTintInfo = new TintInfo();
+            }
+            mTintInfo.mHasTintList = true;
+            mTintInfo.mTintList = mTintManager.getColorStateList(resId);
+        }
+        return applySupportDrawableTint();
+    }
+
+    private void setSupportDrawableTintMode(PorterDuff.Mode mode) {
+        if (mode != null) {
+            if (mTintInfo == null) {
+                mTintInfo = new TintInfo();
+            }
+            mTintInfo.mHasTintMode = true;
+            mTintInfo.mTintMode = mode;
+        }
+    }
+
+    private boolean applySupportDrawableTint() {
+        Drawable drawable = mDrawableCallback.getDrawable();
+        if (drawable != null && mTintInfo != null && mTintInfo.mHasTintList) {
+            Drawable tintDrawable = drawable.mutate();
+            tintDrawable = DrawableCompat.wrap(tintDrawable);
+            if (mTintInfo.mHasTintList) {
+                DrawableCompat.setTintList(tintDrawable, mTintInfo.mTintList);
+            }
+            if (mTintInfo.mHasTintMode) {
+                DrawableCompat.setTintMode(tintDrawable, mTintInfo.mTintMode);
+            }
+            if (tintDrawable.isStateful()) {
+                tintDrawable.setState(mSwitchCompat.getDrawableState());
+            }
+            setDrawable(tintDrawable);
+            if (drawable == tintDrawable) {
+                tintDrawable.invalidateSelf();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private void resetTintResource(int resId/*background resource id*/) {
+        mResId = resId;
+        mTintResId = 0;
+        mTintMode = null;
+        if (mTintInfo != null) {
+            mTintInfo.mHasTintList = false;
+            mTintInfo.mTintList = null;
+            mTintInfo.mHasTintMode = false;
+            mTintInfo.mTintMode = null;
+        }
+    }
+
+    public void tint() {
+        if (mTintResId == 0 || !setSupportDrawableTint(mTintResId)) {
+            Drawable drawable = mTintManager.getDrawable(mResId);
+            if (drawable == null) {
+                drawable = mResId == 0 ? null : ContextCompat.getDrawable(mSwitchCompat.getContext(), mResId);
+            }
+            setDrawable(drawable);
+        }
+    }
+
+    public interface DrawableCallback {
+
+        void setDrawable(Drawable drawable);
+
+        Drawable getDrawable();
+    }
+
+    public interface SwitchCompatExtensible {
+
+        void setTrackTintList(int resId);
+
+        void setTrackTintList(int resId, PorterDuff.Mode mode);
+
+        void setThumbTintList(int resId);
+
+        void setThumbTintList(int resId, PorterDuff.Mode mode);
+    }
+}
---
+++ magicasakura/src/main/java/com/bilibili/magicasakura/widgets/TintSwitchCompat.java
@@ -0,0 +1,340 @@
+package com.bilibili.magicasakura.widgets;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.AnimatedStateListDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.CompoundButtonCompat;
+import android.support.v7.widget.SwitchCompat;
+import android.util.AttributeSet;
+
+import com.bilibili.magicasakura.R;
+import com.bilibili.magicasakura.utils.ThemeUtils;
+import com.bilibili.magicasakura.utils.TintManager;
+
+/**
+ * @author xyczero617@gmail.com
+ * @since 17/5/23
+ */
+
+public class TintSwitchCompat extends SwitchCompat implements Tintable, AppCompatTextHelper.TextExtensible,
+        AppCompatBackgroundHelper.BackgroundExtensible, AppCompatSwitchHelper.SwitchCompatExtensible,
+        AppCompatCompoundButtonHelper.CompoundButtonExtensible {
+
+    private AppCompatBackgroundHelper mBackgroundHelper;
+    private AppCompatCompoundButtonHelper mCompoundButtonHelper;
+    private AppCompatTextHelper mTextHelper;
+    private AppCompatSwitchHelper mThumbHelper;
+    private AppCompatSwitchHelper mTrackHelper;
+
+    public TintSwitchCompat(Context context) {
+        this(context, null);
+    }
+
+    public TintSwitchCompat(Context context, AttributeSet attrs) {
+        this(context, attrs, android.support.v7.appcompat.R.attr.switchStyle);
+    }
+
+    public TintSwitchCompat(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        if (isInEditMode()) {
+            return;
+        }
+        TintManager tintManager = TintManager.get(context);
+        mThumbHelper = new AppCompatSwitchHelper(this, tintManager,
+                new int[]{android.R.attr.thumb, R.attr.thumbTint, R.attr.thumbTintMode},
+                new AppCompatSwitchHelper.DrawableCallback() {
+                    @Override
+                    public void setDrawable(Drawable drawable) {
+                        setThumbDrawable(drawable);
+                    }
+
+                    @Override
+                    public Drawable getDrawable() {
+                        return getThumbDrawable();
+                    }
+                });
+        mThumbHelper.loadFromAttribute(attrs, defStyleAttr);
+
+        mTrackHelper = new AppCompatSwitchHelper(this, tintManager,
+                new int[]{R.attr.track, R.attr.trackTint, R.attr.trackTintMode},
+                new AppCompatSwitchHelper.DrawableCallback() {
+                    @Override
+                    public void setDrawable(Drawable drawable) {
+                        setTrackDrawable(drawable);
+                    }
+
+                    @Override
+                    public Drawable getDrawable() {
+                        return getTrackDrawable();
+                    }
+                });
+        mTrackHelper.loadFromAttribute(attrs, defStyleAttr);
+
+        mBackgroundHelper = new AppCompatBackgroundHelper(this, tintManager);
+        mBackgroundHelper.loadFromAttribute(attrs, defStyleAttr);
+
+        mCompoundButtonHelper = new AppCompatCompoundButtonHelper(this, tintManager);
+        mCompoundButtonHelper.loadFromAttribute(attrs, defStyleAttr);
+
+        mTextHelper = new AppCompatTextHelper(this, tintManager);
+        mTextHelper.loadFromAttribute(attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void drawableStateChanged() {
+        super.drawableStateChanged();
+        if (ThemeUtils.isSkipAnimatedSelector()) {
+            Drawable drawable = CompoundButtonCompat.getButtonDrawable(this);
+            try {
+                if (ThemeUtils.getWrapperDrawable(drawable) instanceof AnimatedStateListDrawable) {
+                    drawable.jumpToCurrentState();
+                }
+            } catch (NoClassDefFoundError error) {
+                error.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void setThumbDrawable(Drawable thumb) {
+        super.setThumbDrawable(thumb);
+        if (mThumbHelper != null) {
+            mThumbHelper.setDrawable();
+        }
+    }
+
+    @Override
+    public void setThumbResource(int resId) {
+        if (mThumbHelper != null) {
+            mThumbHelper.setDrawableId(resId);
+        } else {
+            super.setThumbResource(resId);
+        }
+    }
+
+    @Override
+    public void setThumbTintList(@Nullable ColorStateList tint) {
+        if (mThumbHelper != null) {
+            mThumbHelper.setDrawableTintList(tint);
+        } else {
+            super.setThumbTintList(tint);
+        }
+    }
+
+    @Override
+    public void setThumbTintMode(@Nullable PorterDuff.Mode tintMode) {
+        if (mThumbHelper != null) {
+            mThumbHelper.setDrawableTintMode(tintMode);
+        } else {
+            super.setThumbTintMode(tintMode);
+        }
+    }
+
+    @Override
+    public void setThumbTintList(int resId) {
+        if (mThumbHelper != null) {
+            mThumbHelper.setButtonDrawableTintList(resId, null);
+        }
+    }
+
+    @Override
+    public void setThumbTintList(int resId, PorterDuff.Mode mode) {
+        if (mThumbHelper != null) {
+            mThumbHelper.setButtonDrawableTintList(resId, mode);
+        }
+    }
+
+    @Override
+    public void setTrackDrawable(Drawable track) {
+        super.setTrackDrawable(track);
+        if (mTrackHelper != null) {
+            mTrackHelper.setDrawable();
+        }
+    }
+
+    @Override
+    public void setTrackResource(int resId) {
+        if (mTrackHelper != null) {
+            mTrackHelper.setDrawableId(resId);
+        } else {
+            super.setTrackResource(resId);
+        }
+    }
+
+    @Override
+    public void setTrackTintList(@Nullable ColorStateList tint) {
+        if (mTrackHelper != null) {
+            mTrackHelper.setDrawableTintList(tint);
+        } else {
+            super.setTrackTintList(tint);
+        }
+    }
+
+    @Override
+    public void setTrackTintMode(@Nullable PorterDuff.Mode tintMode) {
+        if (mTrackHelper != null) {
+            mTrackHelper.setDrawableTintMode(tintMode);
+        } else {
+            super.setTrackTintMode(tintMode);
+        }
+    }
+
+    @Override
+    public void setTrackTintList(int resId) {
+        if (mTrackHelper != null) {
+            mTrackHelper.setButtonDrawableTintList(resId, null);
+        }
+    }
+
+    @Override
+    public void setTrackTintList(int resId, PorterDuff.Mode mode) {
+        if (mTrackHelper != null) {
+            mTrackHelper.setButtonDrawableTintList(resId, mode);
+        }
+    }
+
+    @Override
+    public void setTextColor(int color) {
+        super.setTextColor(color);
+        if (mTextHelper != null) {
+            mTextHelper.setTextColor();
+        }
+    }
+
+    @Override
+    public void setTextColor(ColorStateList colors) {
+        super.setTextColor(colors);
+        if (mTextHelper != null) {
+            mTextHelper.setTextColor();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    public void setTextAppearance(int resId) {
+        super.setTextAppearance(resId);
+        if (mTextHelper != null) {
+            mTextHelper.setTextAppearanceForTextColor(resId);
+        }
+    }
+
+    @Override
+    public void setTextAppearance(Context context, int resId) {
+        super.setTextAppearance(context, resId);
+        if (mTextHelper != null) {
+            mTextHelper.setTextAppearanceForTextColor(resId);
+        }
+    }
+
+    @Override
+    public void setBackgroundDrawable(Drawable background) {
+        super.setBackgroundDrawable(background);
+        if (mBackgroundHelper != null) {
+            mBackgroundHelper.setBackgroundDrawableExternal(background);
+        }
+    }
+
+    @Override
+    public void setBackgroundResource(int resId) {
+        if (mBackgroundHelper != null) {
+            mBackgroundHelper.setBackgroundResId(resId);
+        } else {
+            super.setBackgroundResource(resId);
+        }
+    }
+
+    @Override
+    public void setBackgroundColor(int color) {
+        super.setBackgroundColor(color);
+        if (mBackgroundHelper != null) {
+            mBackgroundHelper.setBackgroundColor(color);
+        }
+    }
+
+    @Nullable
+    @Override
+    public void setButtonDrawable(Drawable drawable) {
+        super.setButtonDrawable(drawable);
+        if (mCompoundButtonHelper != null) {
+            mCompoundButtonHelper.setButtonDrawable();
+        }
+    }
+
+    @Override
+    public void setButtonDrawable(@DrawableRes int resId) {
+        if (mCompoundButtonHelper != null) {
+            mCompoundButtonHelper.setButtonDrawable(resId);
+        } else {
+            super.setButtonDrawable(resId);
+        }
+    }
+
+    @Override
+    public int getCompoundPaddingLeft() {
+        final int value = super.getCompoundPaddingLeft();
+        return mCompoundButtonHelper != null
+                ? mCompoundButtonHelper.getCompoundPaddingLeft(value)
+                : value;
+    }
+
+    @Override
+    public void setBackgroundTintList(int resId) {
+        if (mBackgroundHelper != null) {
+            mBackgroundHelper.setBackgroundTintList(resId, null);
+        }
+    }
+
+    @Override
+    public void setBackgroundTintList(int resId, PorterDuff.Mode mode) {
+        if (mBackgroundHelper != null) {
+            mBackgroundHelper.setBackgroundTintList(resId, mode);
+        }
+    }
+
+    @Override
+    public void setCompoundButtonTintList(int resId) {
+        if (mCompoundButtonHelper != null) {
+            mCompoundButtonHelper.setButtonDrawableTintList(resId, null);
+        }
+    }
+
+    @Override
+    public void setCompoundButtonTintList(int resId, PorterDuff.Mode mode) {
+        if (mCompoundButtonHelper != null) {
+            mCompoundButtonHelper.setButtonDrawableTintList(resId, mode);
+        }
+    }
+
+    @Override
+    public void setTextColorById(@ColorRes int colorId) {
+        if (mTextHelper != null) {
+            mTextHelper.setTextColorById(colorId);
+        }
+    }
+
+    @Override
+    public void tint() {
+        if (mTextHelper != null) {
+            mTextHelper.tint();
+        }
+        if (mCompoundButtonHelper != null) {
+            mCompoundButtonHelper.tint();
+        }
+        if (mBackgroundHelper != null) {
+            mBackgroundHelper.tint();
+        }
+        if (mTrackHelper != null) {
+            mTrackHelper.tint();
+        }
+        if (mThumbHelper != null) {
+            mThumbHelper.tint();
+        }
+    }
+}
